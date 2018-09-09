741c7b8f5b6302fd5ede36529fa8511949c3e344
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-09-09 07:54:29 -0400

    initial checkin

---
+++ library_project/urls.py
@@ -0,0 +1,10 @@
+# library_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('books.urls')),
+    path('api/', include('api.urls')),
+]
